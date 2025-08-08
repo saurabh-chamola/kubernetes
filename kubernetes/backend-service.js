5014f5506ca83276fd701af524f8d37af9c51743
saurabh-chamola <117541654+saurabh-chamola@users.noreply.github.com>
2025-08-08 10:50:54 -0700

    Update backend-service.js

--- kubernetes/backend-service.js
+++ kubernetes/backend-service.js
@@ -1,1 +1,16 @@
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: backend-service
+  namespace: wanderlust
+spec:
+  type: NodePort
+  selector:
+    app: backend
+  ports:
+    - port: 8080         # Service port (used inside the cluster)
+      targetPort: 8005   # Container port(in which application/container is runnng)
+      protocol: TCP
+      nodePort: 31102    # New node port (ensure it's between 30000–32767 and not used elsewhere)
 
